e00049506f4c0ea9cb95735961561235f13ecb7a
AlexisNguemby <alexis.nguemby-mbina@laplateforme.io>
2025-01-13 16:19:50 +0100

    job1

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+$str="LaPlateforme";
+$str2="vive";
+$str3="!";
+echo $str2 . " " . $str . " " . $str3;
+
+$val = 6;
+echo $val;
+
+$val += 4;
+echo $val;
+
+
+$myBool = true;
+
+echo "La valeur initiale de myBool est : " . ($myBool ? 'true' : 'false'); 
+
+$myBool = false;
+
+echo "La nouvelle valeur de myBool est : " . ($myBool ? 'true' : 'false');
+
+?>
